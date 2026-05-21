427afbc705626d014600792a8bb140e6984a61ba
jaeheon.kim <jaeheon.kim@placen.co.kr>
2026-05-22 02:08:45 +0900

    remove env override in next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://jjybjmzhnejrymfwftbt.supabase.co',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-  },
 };
 
 export default nextConfig;
